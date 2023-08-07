6760d8a177ee8400277fcbcef4824b15cbe28215
Nick Grover <nickgrover@Nicks-MacBook-Pro.local>
2023-08-07 17:02:26 -0400

    Review database

--- database/tenmo.sql
+++ database/tenmo.sql
@@ -1,18 +1,22 @@
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS tenmo_user, account, transfer, account_transfer;
+
 DROP SEQUENCE IF EXISTS seq_user_id, seq_account_id, seq_transfer_id;
+
 -- Sequence to start user_id values at 1001 instead of 1
 CREATE SEQUENCE seq_user_id
   INCREMENT BY 1
   START WITH 1001
   NO MAXVALUE;
+  
 CREATE TABLE tenmo_user (
 	user_id int NOT NULL DEFAULT nextval('seq_user_id'),
-	username varchar(50) NOT NULL,
+	username varchar(55) NOT NULL,
 	password_hash varchar(200) NOT NULL,
 	CONSTRAINT PK_tenmo_user PRIMARY KEY (user_id),
 	CONSTRAINT UQ_username UNIQUE (username)
 );
+
 -- Sequence to start account_id values at 2001 instead of 1
 -- Note: Use similar sequences with unique starting values for additional tables
 CREATE SEQUENCE seq_account_id
@@ -31,6 +35,7 @@ CREATE SEQUENCE seq_transfer_id
   INCREMENT BY 1
   START WITH 3001
   NO MAXVALUE;
+  
 CREATE TABLE transfer (
 	transfer_id int NOT NULL DEFAULT nextval('seq_transfer_id'),
 	from_user varchar(50) NOT NULL,
